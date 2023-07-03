e1b4ae37893b3f845c0ae29f0e12b5be50c8fdf4
Sowltest <116002001+Sowltest@users.noreply.github.com>
2023-07-03 11:54:26 +0200

    Update groupedAssertions.java

--- src/test/java/com/reytech/demo/groupedAssertions.java
+++ src/test/java/com/reytech/demo/groupedAssertions.java
@@ -1,5 +1,5 @@
 package com.reytech.demo;
-import org.junit.juptier.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
